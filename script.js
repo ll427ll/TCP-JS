13103915ddb366990feeb031f768bba2c4558aa1
kimminseo <gimg51747gmail.com>
2023-05-06 17:00:58 +0900

    null과 undefined

--- script.js
+++ script.js
@@ -96,3 +96,9 @@ typeof true; // boolean 타입
 typeof undefined; // undefined 타입
 typeof null; // object 타입
 
+var num; //초기화하지 않았으므로 undefined 값을 반환함.
+var str = null; // object 타입의 null 값
+typeof secondNum; // 정의되지 않은 변수에 접근하면 undefined 값을 반환함.
+null == undefined; // true
+null === undefined; // false
+
