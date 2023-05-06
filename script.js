7f0d7a0fe298d8120e54885bcc7103f6820beccf
kimminseo <gimg51747gmail.com>
2023-05-06 18:03:34 +0900

    증감 연산자

--- script.js
+++ script.js
@@ -159,6 +159,10 @@ y -= 5; // y = y - 5 와 같은 표현임.
 z =- 5; // z = -5 와 같은 표현임.
 
 var x = 10, y = 10;
+document.write((++x - 3) + "<br>"); // x의 값을 우선 1 증가시킨 후에 3을 뺌.
+document.write(x + "<br>"); // 11
+document.write((y++ - 3) + "<br>"); // 먼저 y에서 3을 뺀 후에 y의 값을 1 증가시킴.
+document.write(y); // 11
 
 
 
