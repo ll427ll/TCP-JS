3881b44a44617053185f5741c4868d37aa8f4d1d
kimminseo <gimg51747gmail.com>
2023-05-06 15:39:42 +0900

    자바스크립트 문법

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 var x = 10;
 var result = x + 3;
+
+
+var javascript = 10; // 변수 javascript와 JavaScript는 서로 다른 두 개의 변수로 인식됨.
+var JavaScript = 20;
+var Script = 30; // 변수의 선언은 var 키워드로만 할 수 있으면 Var는 동작하지 않음.
+
+
