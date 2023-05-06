b79f6f26b1e61e22e3e765ca53571df13b9fe931
kimminseo <gimg51747gmail.com>
2023-05-06 16:23:01 +0900

    내부 자바스크립트

--- script.js
+++ script.js
@@ -49,4 +49,5 @@ str.innerHTML = "이 문장으로 바뀌었습니다!!";
 //script
 document.write(4*5);
 
-<button onclick="document.(4*5)">버튼을 눌러주세요.</button>
+//script
+document.getElementById("text").innerHTML = "여러분들을 환영합니다!";
