af362662321a043f2c8ab9e24cd8f745b010dd85
kimminseo <gimg51747gmail.com>
2023-05-06 15:50:54 +0900

    식별자 작성 방식

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var x = 10;
 var result = x + 3;
 
 
-var javascript = 10; // 변수 javascript와 JavaScript는 서로 다른 두 개의 변수로 인식됨.
+var javascript = 10;
 var JavaScript = 20;
 var Script = 30; // 변수의 선언은 var 키워드로만 할 수 있으면 Var는 동작하지 않음.
 
@@ -11,3 +11,9 @@ var Script = 30; // 변수의 선언은 var 키워드로만 할 수 있으면 Va
 "JavaScript" // 문자열 리터럴
 "안녕하세요" // 문자열 리터럴
 true // 불리언 리터럴
+
+
+var firstVar = 10; // Camel Case
+function my_first_func() { // Underscore Case 방식
+    var firstLocal = 20; // Camel Case 방식
+}
