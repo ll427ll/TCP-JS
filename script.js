a2bdcf2cc38af5502f82feecc2114a0dd8ee4a08
kimminseo <gimg51747gmail.com>
2023-05-06 16:07:30 +0900

    window.alert() 메소드

--- script.js
+++ script.js
@@ -42,6 +42,6 @@ document.getElementById("text").innerHTML = x;
 
 <script>
     function alerDialog Box() {
-        alert ("확인을 누를 때까지 다른 작업을 할 수 없어요!");
+        alert ("확인을 누를 때까지 다른 작업을 할 수 없어요!")
     }
 </script>
